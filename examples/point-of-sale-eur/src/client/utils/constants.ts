24a98c73a6c2c1b187bf0884c56b67c7993434a9
MattBooth-UK <151445325+MattBooth-UK@users.noreply.github.com>
2024-05-21 16:07:18 +0100

    Update constants.ts

--- examples/point-of-sale-eur/src/client/utils/constants.ts
+++ examples/point-of-sale-eur/src/client/utils/constants.ts
@@ -13,10 +13,6 @@ export const MAINNET_ENDPOINT = 'https://solana-mainnet.phantom.tech';
 // Mint DUMMY tokens on devnet @ https://spl-token-faucet.com
 export const DEVNET_DUMMY_MINT = new PublicKey('Gh9ZwEmdLJ8DscKNTkTqPbNwLNNBjuSzaG9Vp2KGtKJr');
 
-export const MAINNET_USDC_MINT = new PublicKey('EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v');
-
 export const MAINNET_EURC_MINT = new PublicKey('HzwqbKZw8HxMN6bF2yFZNrht3c2iXXzpKcFu7uBEDKtr');
-
-export const DEVNET_USDC_MINT = new PublicKey('4zMMC9srt5Ri5X14GAgXhaHii3GnPAEERYPJgZJDncDU');
 
 export const DEVNET_EURC_MINT = new PublicKey('HzwqbKZw8HxMN6bF2yFZNrht3c2iXXzpKcFu7uBEDKtr');
